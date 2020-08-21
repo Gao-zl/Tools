3c0109e375c23f0a97843d02dfa983b0f03e2e7b
Gao-zl <15860280097@163.com>
2020-08-21 21:57:37 +0800

    Passwd1.2

--- password/strongPasswd_py2.py
+++ password/strongPasswd_py2.py
@@ -1,20 +1,37 @@
-from os import urandom
-from random import choice
+#/user/bin/python
+# -*- coding: utf-8 -*-
+"""
+Version:    V1.0
+Time:       2020.08.21
+Author:     Gaozhl
+使用的是Python2来运行
+"""
+import os
+import random
 
+# 去掉#防止出现密码被注释的情况
 char_set = {'small': 'abcdefghijklmnopqrstuvwxyz',
              'nums': '0123456789',
              'big': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-             'special': '^!\$%&/()=?{[]}+~#-.:,;<>|'
+             'special': '^!\$%&/()=?{[]}+~-.:,;<>|'
             }
 
 
 def generate_pass(length=21):
-    """Function to generate a password"""
+    """
+    Function to generate a password
+    修改：以大写字母开头，防止某些时刻的特殊要求
+    :param length:  生成密码长度
+    :return:        生成的密码结果
+    """
     password = []
+    # 先从24字母中获取一个值
+    i = random.randint(0, 25)
+    password.append(char_set['big'][i])
 
     while len(password) < length:
-        key = choice(list(char_set.keys()))
-        a_char = urandom(1)
+        key = random.choice(char_set.keys())
+        a_char = os.urandom(1)
         if a_char in char_set[key]:
             if check_prev_char(password, char_set[key]):
                 continue
@@ -24,8 +41,14 @@ def generate_pass(length=21):
 
 
 def check_prev_char(password, current_char_set):
-    """Function to ensure that there are no consecutive
-    UPPERCASE/lowercase/numbers/special-characters."""
+    """
+    Function to ensure that there are no consecutive
+    UPPERCASE/lowercase/numbers/special-characters.
+    确保两个字符不是属于相同的类型
+    :param password:            输入的密码
+    :param current_char_set:    当前的密码字符的种类
+    :return:                    放回True或False
+    """
     index = len(password)
     if index == 0:
         return False
@@ -36,4 +59,5 @@ def check_prev_char(password, current_char_set):
         else:
             return False
 
-print((generate_pass()))
+# 测试
+# print((generate_pass(24)))
